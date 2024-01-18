5e9011aa733a43695b01bb4db732a89f01d555de
pytbyte <heretolearn1@gmail.com>
2024-01-19 00:48:23 +0300

    MIN OPS

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,39 +1,37 @@
 #!/usr/bin/python3
-'''Minimum operations coding challenge.
+'''Minimum operations interview.
 '''
 
 
-def minOperations(n):
+def min_operations_to_generate(target_count):
     '''
-    Computes the fewest number of operations needed to result
-    in exactly n H characters using Copy All and Paste operations.
+    Computes the fewest number of operations needed to generate
+    exactly target_count H characters using Copy All and Paste operations.
     '''
-    if not isinstance(n, int) or n <= 0:
+    if not isinstance(target_count, int) or target_count <= 0:
         # Invalid input, return 0 as it's impossible to achieve.
         return 0
 
-    operations_count = 0  # To keep track of the total number of operations.
-    clipboard = 0  # The content in the clipboard that can be pasted.
-    characters_generated = 1  # The number of 'H' characters generated.
+    total_operations = 0
+    clipboard_content = 0
+    characters_generated = 1
 
     # Perform operations until the desired number of characters is achieved.
-    while characters_generated < n:
-        if clipboard == 0:
+    while characters_generated < target_count:
+        if clipboard_content == 0:
             # If clipboard is empty, perform an initial Copy All and Paste.
-            clipboard = characters_generated
-            characters_generated += clipboard
-            operations_count += 2
-        elif (n - characters_generated) % characters_generated == 0:
-            # If the remaining characters can be evenly divided by the
-            # current count,
-            # perform a Copy All and Paste.
-            clipboard = characters_generated
-            characters_generated += clipboard
-            operations_count += 2
-        elif clipboard > 0:
+            clipboard_content = characters_generated
+            characters_generated += clipboard_content
+            total_operations += 2
+        elif (target_count - characters_generated) % characters_generated == 0:
+            # If the remaining characters can be evenly divided,
+            # perform another Copy All and Paste.
+            clipboard_content = characters_generated
+            characters_generated += clipboard_content
+            total_operations += 2
+        elif clipboard_content > 0:
             # Perform a Paste operation.
-            characters_generated += clipboard
-            operations_count += 1
+            characters_generated += clipboard_content
+            total_operations += 1
 
-    return operations_count
-
+    return total_operations
