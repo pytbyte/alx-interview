6d97e170eb9acacf3f8958403e4844d894f303fa
pytbyte <heretolearn1@gmail.com>
2024-01-19 00:52:02 +0300

    MIN OPS

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,12 +3,12 @@
 '''
 
 
-def min_operations_to_generate(target_count):
+def min_operations_to_generate(n):
     '''
     Computes the fewest number of operations needed to generate
-    exactly target_count H characters using Copy All and Paste operations.
+    exactly n H characters using Copy All and Paste operations.
     '''
-    if not isinstance(target_count, int) or target_count <= 0:
+    if not isinstance(n, int) or n <= 0:
         # Invalid input, return 0 as it's impossible to achieve.
         return 0
 
@@ -17,13 +17,13 @@ def min_operations_to_generate(target_count):
     characters_generated = 1
 
     # Perform operations until the desired number of characters is achieved.
-    while characters_generated < target_count:
+    while characters_generated < n:
         if clipboard_content == 0:
             # If clipboard is empty, perform an initial Copy All and Paste.
             clipboard_content = characters_generated
             characters_generated += clipboard_content
             total_operations += 2
-        elif (target_count - characters_generated) % characters_generated == 0:
+        elif (n - characters_generated) % characters_generated == 0:
             # If the remaining characters can be evenly divided,
             # perform another Copy All and Paste.
             clipboard_content = characters_generated
