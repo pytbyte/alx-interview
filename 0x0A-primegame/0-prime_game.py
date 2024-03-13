b0f3ea2ed7c7654e9fbedeeba487d27b427fa764
pytbyte <heretolearn1@gmail.com>
2024-03-13 15:02:32 +0300

    Prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """ Module for solving prime game question """
 
+
 def isWinner(x_rounds, nums):
     """
     Determine the winner of the prime game.
 
     Args:
         x_rounds (int): The number of rounds to play.
-        nums (list): A list of integers representing the values of 'n' for each round.
+        nums (list): A list of integers
+        representing the values of 'n' for each round.
 
     Returns:
-        str or None: The name of the player that won the most rounds. Returns 'Maria',
-                     'Ben', or None if the winner cannot be determined.
+        str or None: The name of the player that
+        won the most rounds. Returns 'Maria',
+        'Ben', or None if the winner cannot be determined.
     """
     if not nums or x_rounds < 1:
         return None
